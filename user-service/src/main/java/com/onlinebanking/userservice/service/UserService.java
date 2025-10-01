0c84cac06d8d04a666f4d2882a373dac4e51b651
Spydiecy <spydiecy@gmail.com>
2025-10-01 12:34:56 +0530

    fixed stuff

--- user-service/src/main/java/com/onlinebanking/userservice/service/UserService.java
+++ user-service/src/main/java/com/onlinebanking/userservice/service/UserService.java
@@ -5,12 +5,18 @@ import com.onlinebanking.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -21,11 +27,28 @@ public class UserService {
         if (userRepository.findAll().stream().anyMatch(u -> u.getAccountNumber().equals(accountNumber))) {
             return false;
         }
+        
+        // Create user
         User user = new User();
         user.setUsername(username);
         user.setPassword(passwordEncoder.encode(password));
         user.setAccountNumber(accountNumber);
         userRepository.save(user);
+        
+        // Create corresponding account in Account Service
+        try {
+            Map<String, Object> accountData = new HashMap<>();
+            accountData.put("accountNumber", accountNumber);
+            accountData.put("accountHolderName", username);
+            accountData.put("balance", 0.0); // Initial balance
+            accountData.put("username", username);
+            
+            restTemplate.postForObject("http://localhost:8081/accounts", accountData, Object.class);
+        } catch (Exception e) {
+            System.err.println("Failed to create account in Account Service: " + e.getMessage());
+            // Continue - user creation successful, account creation can be retried later
+        }
+        
         return true;
     }
 
